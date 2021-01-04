0b2ebfa2b6fe19c9bed2a667beaecfdc3b570a9b
you-know-who-2017 <1376397133@qq.com>
2021-01-04 17:05:19 +0800

    update

--- osTest/test1/test1.c
+++ osTest/test1/test1.c
@@ -1,16 +1,19 @@
 /*
  * @Author: geekli
  * @Date: 2021-01-04 16:59:50
- * @LastEditTime: 2021-01-04 16:59:59
+ * @LastEditTime: 2021-01-04 17:03:21
  * @LastEditors: your name
  * @Description: 
  * @FilePath: /OS_Test/osTest/test1/test1.c
  */
-void main(){  
-   int pid1=fork();  
-   printf(“**1**\n”);  
-   int pid2=fork();  
-   printf(“**2**\n”);  
-   if(pid1==0){int pid3=fork();printf(“**3**\n”);}  
-   else printf(“**4**\n”);  
-}  
+void main(){
+int pid1=fork();
+printf("**1**\n");
+int pid2=fork();
+printf("**2**\n");
+if(pid1==0){
+    int pid3=fork();
+    printf("**3**\n");
+}
+else printf("**4**\n");
+}
